67b8e8c838129ff08d917e3ff5a89b7eb4fa034c
Gamu <gamu.development@gmail.com>
2022-07-10 20:24:51 +0200

    Added: KeyViewModel to observe data changes from language keys collections

---
+++ WpfApp1/Models/KeyViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+
+namespace WpfApp1.Models
+{
+    public class KeyViewModel
+    {
+        public ObservableCollection<KeyModel> Keys { get; private set; }
+
+        public KeyViewModel() => Keys = new ObservableCollection<KeyModel>();
+    }
+}
